c57f6dec93152da73fef2631a60464ae4a1d1cef
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2021-06-09 16:00:01 +0700

    adds status and metrics endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/status")
+def status():
+    return {"data": {"UserCount": 140, "UserCountActive": 23}}
+
+
+@app.route("/metrics")
+def metrics():
+    return {"result": "OK - healthy"}
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
