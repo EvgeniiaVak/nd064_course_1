d3705a57996034952d019cf4b157b5462ef701b4
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2021-06-09 17:09:50 +0700

    adds logger

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,41 @@
 from flask import Flask
+from flask import request
+from logging.config import dictConfig
+
+
+dictConfig({
+    'version': 1,
+    'formatters':
+    {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s: %(message)s',
+        }
+    },
+    'handlers':
+    {
+        'wsgi':
+        {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'app.log',
+            'maxBytes': 1024,
+            'backupCount': 3,
+            'formatter': 'default'
+        }
+    },
+    'root':
+    {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
 app = Flask(__name__)
+
+
+@app.before_request
+def log_request_info():
+    app.logger.debug(f'[{request.path}] endpoint was reached')
 
 
 @app.route("/")
